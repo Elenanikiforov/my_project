ecba91fabc4bf4aedc02d0fcf9fd1413c6e49ce3
elena nikiforova <elena.nikiforova.99@bk.ru>
2019-04-15 11:09:23 +0300

    init commit

---
+++ get_parity.py
@@ -0,0 +1,5 @@
+def ch(a):
+    if a%2==0:
+        print('Четное')
+    else:
+        print('Нечетное')
---
+++ table.py
@@ -0,0 +1,13 @@
+
+n = int(input("Введите номер хим элемента: "))
+if n == 25:
+    print("Mn, марганец")
+elif n == 3:
+    print("Li,Литий")
+elif n == 80:
+    print("Hg,Ртуть")
+elif n == 17:
+    print("Cl,Хлор")
+else:
+    print("Неизвестный элемент")
+
